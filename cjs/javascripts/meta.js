10dcd7f733db392716df9665ef69eca0182095e0
Staz0r <51358327+Staz0r@users.noreply.github.com>
2021-06-26 00:51:13 +0700

    Changed to storage

--- cjs/javascripts/meta.js
+++
@@ -1,4 +0,0 @@
-var meta = document.createElement('meta');
-meta.http-equiv = "refresh";
-meta.content = "2;url= perdit-io.github.io/cuddly-eureka/";
-document.getElementsByTagName('head')[0].appendChild(meta);
